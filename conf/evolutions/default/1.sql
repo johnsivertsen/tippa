e1e759e08f7b65ee04a481a0ee3995183b35fcdc
John Sivertsen <john@sivertsen.fo>
2016-07-17 11:06:16 +0100

    add ddl and model classes

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -2,6 +2,148 @@
 
 # --- !Ups
 
+-- user
+create table user (
+  id integer primary key auto_increment,
+  e_mail varchar not null,
+  password varchar not null,
+  first_name varchar not null,
+  last_name varchar not null,
+  created_date timestamp not null default now(),
+  status varchar not null
+);
+
+-- tournament
+create table tournament (
+  id integer primary key auto_increment,
+  short_name varchar not null,
+  full_name varchar,
+  type varchar not null default 'football',
+  created_date timestamp not null default now()
+);
+
+-- round
+create table round (
+  id integer primary key auto_increment,
+  id_tournament integer not null,
+  number integer not null,
+  designated_date timestamp not null,
+  created_date timestamp not null default now(),
+  foreign key (id_tournament) references tournament(id),
+  unique (id_tournament, number)
+);
+
+-- user_tournament
+create table user_tournament (
+  id integer primary key auto_increment,
+  id_user integer not null,
+  id_tournament integer not null,
+  foreign key (id_user) references user(id),
+  foreign key (id_tournament) references tournament(id),
+  unique (id_user, id_tournament)
+);
+
+-- team
+create table team (
+  id integer primary key auto_increment,
+  short_name varchar not null,
+  long_name varchar,
+  description varchar,
+  type varchar not null default 'football',
+  country varchar not null default 'INTERNATIONAL',
+  created_date timestamp not null default now()
+);
+
+-- tournament_team
+create table tournament_team (
+  id integer primary key auto_increment,
+  id_tournament integer,
+  id_team integer,
+  created_date timestamp not null default now(),
+  foreign key (id_tournament) references tournament(id),
+  foreign key (id_team) references team(id),
+  unique (id_tournament, id_team)
+);
+
+-- fixture
+create table fixture (
+  id integer primary key auto_increment,
+  id_tournament integer not null,
+  id_round integer not null,
+  id_team_home integer not null,
+  id_team_away integer not null,
+  home_points integer,
+  away_points integer,
+  home_points_awarded integer,
+  away_points_awarded integer,
+  start_time timestamp,
+  status varchar,
+  created_date timestamp not null default now(),
+  foreign key (id_tournament) references tournament(id),
+  foreign key (id_team_home) references team(id),
+  foreign key (id_team_away) references team(id),
+  unique (id_tournament, id_round, id_team_home),
+  unique (id_tournament, id_round, id_team_away),
+  check (id_team_home <> id_team_away)
+);
+
+-- bet
+create table bet (
+  id integer primary key auto_increment,
+  id_fixture integer not null,
+  id_user integer not null,
+  home_points integer,
+  away_points integer,
+  created_date timestamp not null default now(),
+  foreign key (id_fixture) references fixture(id),
+  foreign key (id_user) references user(id),
+  unique (id_fixture, id_user)
+);
+
+-- friend_group
+create table friend_group (
+  id integer primary key auto_increment,
+  admin_user integer not null,
+  name varchar not null,
+  description varchar,
+  status varchar,
+  created_date timestamp not null default now(),
+  foreign key (admin_user) references user(id)
+);
+
+-- user_group
+create table friend_group_user (
+  id integer primary key auto_increment,
+  id_user integer not null,
+  id_friend_group integer not null,
+  status varchar,
+  created_date timestamp not null default now(),
+  foreign key (id_user) references user(id),
+  foreign key (id_friend_group) references friend_group(id),
+  unique (id_user, id_friend_group)
+);
+
+-- message
+create table message (
+  id integer primary key auto_increment,
+  recipient_id integer not null,
+  type varchar,
+  message varchar,
+  status varchar,
+  created_date timestamp not null default now()
+);
+
+-- log
+create table log (
+  id integer primary key auto_increment,
+  key_1 integer,
+  key_2 integer,
+  type varchar,
+  message varchar,
+  created_date timestamp not null default now()
+);
+
+-- dummy table and data:
 CREATE TABLE "order" (
 	id INTEGER PRIMARY KEY AUTO_INCREMENT,
 	date TIMESTAMP NOT NULL DEFAULT now(),
@@ -14,4 +156,18 @@ INSERT INTO "order" (name, description) VALUES ('Pixel', 'Google Pixel LS 2015')
 
 # --- !Downs
 
+drop table log;
+drop table message;
+drop table user_group;
+drop table friend_group;
+drop table bet;
+drop table fixture;
+drop table tournament_team;
+drop table team;
+drop table user_tournament;
+drop table round;
+drop table tournament;
+drop table user;
+
+-- dummy table and data:
 DROP TABLE "order";
